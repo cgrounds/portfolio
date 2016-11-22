c38d65a4f772e9c2f62780f0c4c89e11a56c2df5
Clint Grounds <clintgrounds@gmail.com>
2016-11-22 15:58:41 -0600

    fixed images

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,10 +4,10 @@ class Portfolio extends Component {
   render(){
     return(
       <div id="portfolio">
-      <a href="https://bakertx.github.io/travel-pins-front/"><img src="../images/travelpins.png" role="presentation"/></a>
-      <a target="_blank" href="http://www.lillalane.com"><img src="../images/lillalane.png" role="presentation"/></a>
-      <a target="_blank" href="https://www.clintgrounds.com"><img src="../images/clintgroundswebsite.png" role="presentation"/></a>
-      <a target="_blank" href="https://bakertx.github.io/sandbox/"><img src="../images/sandbox.png" role="presentation"/></a>
+      <a href="https://bakertx.github.io/travel-pins-front/"><img src={require('../public/images/travelpins.png')} role="presentation"/></a>
+      <a target="_blank" href="http://www.lillalane.com"><img src={require('../public/images/lillalane.png')} role="presentation"/></a>
+      <a target="_blank" href="https://www.clintgrounds.com"><img src={require('../public/images/clintgroundswebsite.png')} role="presentation"/></a>
+      <a target="_blank" href="https://bakertx.github.io/sandbox/"><img src={require('../public/images/sandbox.png')} role="presentation"/></a>
       </div>
     )
   }
