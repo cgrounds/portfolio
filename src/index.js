138dfc0ec4d2efa169db33f24ea30ed09aad5e24
Clint Grounds <clintgrounds@gmail.com>
2016-11-22 12:43:21 -0600

    all changes made

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './CSS/index.css';
 import Home from './Home.js'
 import Contact from './Contact.js'
 import About from './About.js'
